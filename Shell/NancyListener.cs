ec764f30a43887d4f15129a9b431a46988ecea0f
Tristan McAuley <tristan.mcauley@gmail.com>
2018-06-26 23:09:52 +0800

    shell.nancy current.aspnetcore with reverse proxy

---
+++ Shell/NancyListener.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Fabric;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace Shell
+{
+    public class NancyListener : ICommunicationListener
+    {
+        private IDisposable _listener;
+
+        private readonly StatefulServiceContext _context;
+
+        public NancyListener(StatefulServiceContext context)
+        {
+            _context = context;
+        }
+
+        public void Abort()
+        {
+            
+        }
+
+        public Task CloseAsync(CancellationToken cancellationToken)
+        {
+            _listener?.Dispose();
+            return Task.CompletedTask;
+        }
+
+        public Task<string> OpenAsync(CancellationToken cancellationToken)
+        {
+
+            var endpoint = _context.CodePackageActivationContext.GetEndpoint("ServiceEndpoint");
+            int port = endpoint.Port;
+
+            var listeningAt = $@"http://+:{port}/";
+
+            var opts = new StartOptions($@"http://*:{port}");
+            _listener = WebApp.Start<Startup>(listeningAt);
+
+            var publishAt = listeningAt.Replace("+", FabricRuntime.GetNodeContext().IPAddressOrFQDN);
+
+            ServiceEventSource.Current.Message($"Starting owin on {publishAt}");
+
+            return Task.FromResult(publishAt);
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
---
+++ Shell/TestModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell
+{
+    public class TestModule : NancyModule
+    {
+        public TestModule()
+        {
+            Get["tests"] = args => "Hit tests!";
+        }
+    }
+}
